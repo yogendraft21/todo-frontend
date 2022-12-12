19668ecd927c003707cbf674a01a6db1c56d0941
yogendraft21 <yogendra311204@gmail.com>
2022-12-12 17:10:25 +0530

    frontend

---
+++ script/index.js
@@ -0,0 +1,63 @@
+let signup = document.getElementById("signup")
+
+signup.addEventListener("click",()=>{
+    userSignup();
+})
+
+const userSignup=async()=>{
+    const url = "http://localhost:8081/signup"
+
+    let email = document.getElementById("sign_email").value
+    let password = document.getElementById("sign_password").value
+
+    let payload = {
+        email:email,
+        password:password
+    }
+    console.log(payload)
+    const res  = await fetch(url,{
+        method:'POST',
+        body:JSON.stringify(payload),
+        headers:{
+            'Content-type':"application/json"
+        }
+    })
+        const data  = await res.json()
+    console.log(data)
+}
+let login = document.getElementById("login")
+
+login.addEventListener("click",()=>{
+    userlogin();
+})
+
+const userlogin=async()=>{
+    const url = "http://localhost:8081/login"
+
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+
+    let payload = {
+        email:email,
+        password:password
+    }
+    console.log(payload)
+    const res  = await fetch(url,{
+        method:'POST',
+        body:JSON.stringify(payload),
+        headers:{
+            'Content-type':"application/json"
+        }
+    })
+    const data  = await res.json()
+    console.log(data.length)
+    if(data.email!=""){
+
+        localStorage.setItem("token",(data.token))
+        alert("Login Success")
+        location.href="./notes.html"
+    }
+    else{
+        alert("Invalid Creadetial")
+    }
+}
---
+++ script/notes.js
@@ -0,0 +1,120 @@
+const getNotes = async()=>{
+    const url = "http://localhost:8081/todo"
+    const res  =await fetch(url,{
+        headers:{
+            'Content-type':'application/json',
+            Authorization:`Bearer ${localStorage.getItem("token")}`
+        }
+            
+    })
+    const data = await res.json();
+    display(data)
+}
+getNotes();
+
+const display = (data)=>{
+    let note = document.getElementById("note")
+    data.forEach((el)=>{
+        let div = document.createElement("div")
+        let id = document.createElement("h3")
+        id.innerText = el._id;
+        let title = document.createElement("h3")
+        title.innerText = el.taskname;
+        let data  = document.createElement("p")
+        data.innerText = el.status
+        let tag = document.createElement("p")
+        tag.innerText = el.tag;
+        let h = document.createElement("hr")
+        div.append(id,title,data,tag,h)
+        note.append(div)
+    })
+}
+
+const add  = document.getElementById("add")
+add.addEventListener('click',()=>{
+    addTodo();
+})
+
+
+
+const addTodo = async()=>{
+    const url = "http://localhost:8081/todo/create";
+
+    const tname = document.getElementById("taskname").value
+    const status = document.getElementById("status").value
+    const tag = document.getElementById("tag").value
+    const payload = {
+        taskname:tname,
+        status:status,
+        tag:tag
+    }
+    const res = await fetch(url,{
+        method:'POST',
+        body : JSON.stringify(payload),
+        headers:{
+            'Content-type':'application/json',
+            Authorization:`Bearer ${localStorage.getItem("token")}`
+        }
+    })
+    alert("Created")
+    const data = res.json();
+    console.log(data)
+}
+
+
+const del = document.getElementById("del")
+add.addEventListener('click',()=>{
+    delTodo();
+})
+const delTodo = async()=>{
+    // console.log("hello")
+    const id = document.getElementById("todo_id").value;
+    const url = `http://localhost:8081/todo/delete/${id}`;
+    try {
+        const res = await fetch(url,{
+            method:'DELETE',
+            headers:{
+                'Content-type':'application/json',
+                Authorization:`Bearer ${localStorage.getItem("token")}`
+            }
+    })
+   
+    const data = await res.json();
+    alert("Deleted")
+    console.log(data)
+    } catch (error) {
+        console.log("something wrong")
+    }
+    
+}
+const updateTodo = async()=>{
+    // console.log("hello")
+    const id = document.getElementById("todo_id").value;
+    const url = `http://localhost:8081/todo/edit/${id}`;
+
+    const tname = document.getElementById("taskname").value
+    const status = document.getElementById("status").value
+    const tag = document.getElementById("tag").value
+    const payload = {
+        taskname:tname,
+        status:status,
+        tag:tag
+    }
+    try {
+        const res = await fetch(url,{
+            method:'PATCH',
+            body:JSON.stringify(payload),
+            headers:{
+                'Content-type':'application/json',
+                Authorization:`Bearer ${localStorage.getItem("token")}`
+            }
+    })
+   
+    const data = await res.json();
+    console.log(data)
+    alert("Updated")
+    } catch (error) {
+        console.log("something wrong")
+    }
+    
+}
